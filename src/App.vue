3fd47f8822c65fc7dce2c1371b23c671160e4679
besoeasy <besoeasy@users.noreply.github.com>
2023-12-11 11:34:18 +0530

    xd

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,87 @@
 <template>
-		<section class="pt-24 pb-28 bg-white overflow-hidden">
-			<div class="container px-4 mx-auto">
-				<div class="text-center max-w-lg mx-auto">
-					<h2 v-if="rank !== null" class="mb-5 text-6xl md:text-7xl font-bold font-heading text-center tracking-px-n leading-tight">Rank: {{ rank }}</h2>
+	<section class="pt-24 pb-28 bg-white overflow-hidden">
+		<div class="container px-4 mx-auto">
+			<div class="text-center max-w-lg mx-auto">
+				<h2
+					v-if="rank !== null"
+					class="mb-5 text-6xl md:text-7xl font-bold font-heading text-center tracking-px-n leading-tight"
+				>
+					Rank: {{ rank }}
+				</h2>
 
-					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="balance !== null">Balance: {{ balance / 10 ** 8 }}</p>
+				<p class="mb-7 text-lg text-gray-600 font-medium" v-if="balance !== null">
+					Balance: {{ balance / 10 ** 8 }} BTC
+				</p>
 
-					<input class="py-4 px-6 w-full my-10" v-model="address" placeholder="Enter Bitcoin address" />
+				<input class="py-4 px-6 w-full my-10" v-model="address" placeholder="Enter Bitcoin address" />
 
-					<div class="mb-11 md:inline-block">
-						<button
-							@click="getBalance"
-							class="py-4 px-6 w-full text-white font-semibold border border-indigo-700 rounded-xl shadow-4xl focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200"
-							type="button"
-						>
-							Get Rank
-						</button>
-					</div>
-
-					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="bossman !== null">Bossman: {{ bossman }}</p>
+				<div class="mb-11 md:inline-block">
+					<button
+						@click="getBalance"
+						class="py-4 px-6 w-full text-white font-semibold border border-indigo-700 rounded-xl shadow-4xl focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200"
+						type="button"
+					>
+						Get Rank
+					</button>
 				</div>
+
+				<p class="mb-7 text-lg text-gray-600 font-medium" v-if="bossman !== null">
+					Bossman: {{ bossman }} - Boss Balance {{ bossmanBal / 10 ** 8 }} BTC - Bossman By
+					{{ (bossmanBal - balance) / 10 ** 8 }} BTC
+				</p>
 			</div>
-		</section>
+		</div>
+	</section>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import axios from "axios";
+	import { ref } from 'vue';
+	import axios from 'axios';
 
-let balance = ref(null);
-let address = ref("");
-let rank = ref(null);
-let bossman = ref(null);
+	let balance = ref(null);
+	let address = ref('');
+	let rank = ref(null);
+	let bossman = ref(null);
+	let bossmanBal = ref(null);
 
-async function axiosCall(url) {
-	console.log(url);
+	async function axiosCall(url) {
+		console.log(url);
 
-	const response = await axios.get(url);
+		const response = await axios.get(url);
 
-	console.log(response.data);
+		console.log(response.data);
 
-	if (response.data.status === 430) {
-		console.log("430 error");
-		return;
+		if (response.data.status === 430) {
+			console.log('430 error');
+			return;
+		}
+
+		return response.data;
 	}
 
-	return response.data;
-}
+	const getBalance = async () => {
+		if (address.value) {
+			const exp1 = await axiosCall(`https://api.blockchair.com/bitcoin/dashboards/address/${address.value}`);
 
-const getBalance = async () => {
-	if (address.value) {
-		const exp1 = await axiosCall(`https://api.blockchair.com/bitcoin/dashboards/address/${address.value}`);
+			balance.value = exp1.data[address.value].address.balance;
 
-		balance.value = exp1.data[address.value].address.balance;
+			const exp2 = await axiosCall(
+				`https://api.blockchair.com/bitcoin/addresses?a=count()&q=balance(${balance.value}..34859739823342)`
+			);
 
-		const exp2 = await axiosCall(`https://api.blockchair.com/bitcoin/addresses?a=count()&q=balance(${balance.value}..34859739823342)`);
+			rank.value = exp2.data[0]['count()'] || 0;
 
-		rank.value = exp2.data[0]["count()"] || 0;
+			const exp3 = await axiosCall(
+				`https://api.blockchair.com/bitcoin/addresses?q=balance(${
+					balance.value + 1
+				}..34859739823342)&limit=5&s=balance(asc)`
+			);
 
-		const exp3 = await axiosCall(`https://api.blockchair.com/bitcoin/addresses?q=balance(${balance.value + 1}..34859739823342)&limit=5&s=balance(asc)`);
+			bossman.value = exp3.data[0].address;
 
-		bossman.value = exp3.data[0].address;
-	}
-};
+			const exp4 = await axiosCall(`https://api.blockchair.com/bitcoin/dashboards/address/${bossman.value}`);
+
+			bossmanBal.value = exp4.data[bossman.value].address.balance;
+		}
+	};
 </script>

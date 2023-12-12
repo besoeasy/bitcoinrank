06ed5bef9f30570856886393592a8ce54aed3445
besoeasy <besoeasy@users.noreply.github.com>
2023-12-12 13:43:12 +0530

    Refactor App.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
 	<div class="m-auto container">
-		<div class="flex min-h-full	">
+		<div class="flex min-h-screen">
+			<div class="w-1/3 m-auto p-10" v-if="bossman2 !== null">
+				<h2 class="text-xl md:text-3xl font-bold">
+					<div># {{ rank - 2 }}</div>
+				</h2>
+
+				<div class="my-5"></div>
+
+				<img :src="`https://robohash.org/` + bossman2 + `.png?set=set2&size=500x500`" />
+
+				<div class="my-5"></div>
+
+				<input class="p-4 w-full bg-orange-300 hover:bg-orange-400 rounded-lg text-center text-gray-800" v-model="bossman2" />
+
+				<div class="my-5"></div>
+
+				<p class="text-xl font-bold bg-orange-300 p-3 rounded-lg text-center">BTC : {{ bossmanBal2 / 10 ** 8 }}</p>
+			</div>
+
 			<div class="w-1/3 m-auto p-10" v-if="bossman !== null">
 				<h2 class="text-xl md:text-3xl font-bold">
 					<div># {{ rank - 1 }}</div>
@@ -29,22 +47,13 @@
 				<img :src="`https://robohash.org/` + address + `.png?set=set2&size=500x500`" />
 				<div class="my-5"></div>
 
-				<input class="p-4 w-full bg-orange-300 hover:bg-orange-400 rounded-lg text-center text-gray-800" v-model="address" placeholder="Enter Bitcoin address" />
+				<input class="p-4 w-full bg-green-300 hover:bg-orange-400 rounded-lg text-center text-gray-800" v-model="address" placeholder="Enter Bitcoin address" />
 
 				<div class="my-5"></div>
 
-				<p class="text-xl font-bold bg-orange-300 p-3 rounded-lg text-center">BTC : {{ balance / 10 ** 8 }}</p>
+				<p class="text-xl font-bold bg-green-300 p-3 rounded-lg text-center">BTC : {{ balance / 10 ** 8 }}</p>
 
-
-				<button class="mt-5 w-full bg-orange-300 px-4 py-3 rounded-lg text-bold hover:bg-orange-400" @click="getBalance" v-if="!bossmanBal">FETCH</button>
-			</div>
-
-			<div class="w-1/3 m-auto p-10">
-				<div class="text-center max-w-lg mx-auto">
-					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="balance !== null">Balance: {{ (balance / 10 ** 8) * bitcoinPrice }} USD</p>
-
-					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="bitcoinPrice !== null">Bitcoin Price: {{ bitcoinPrice }}</p>
-				</div>
+				<button class="mt-5 w-full bg-green-300 px-4 py-3 rounded-lg text-bold hover:bg-green-400" @click="getBalance" v-if="!bossmanBal">FETCH</button>
 			</div>
 		</div>
 	</div>
@@ -53,13 +62,17 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+let bitcoinPrice = ref(null);
 
 let balance = ref(null);
 let address = ref("");
 let rank = ref(null);
+
 let bossman = ref(null);
 let bossmanBal = ref(null);
-let bitcoinPrice = ref(null);
+
+let bossman2 = ref(null);
+let bossmanBal2 = ref(null);
 
 setInterval(() => {
 	if (Math.random() > 0.5) {
@@ -115,6 +128,10 @@ const getBalance = async () => {
 		bossman.value = exp3.data[0].address;
 
 		bossmanBal.value = await getBitcoinBalance(bossman.value);
+
+		bossman2.value = exp3.data[1].address;
+
+		bossmanBal2.value = await getBitcoinBalance(bossman2.value);
 	}
 };
 </script>

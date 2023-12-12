063dc3c8d6a871d7e06d6893f32871b15913b654
besoeasy <besoeasy@users.noreply.github.com>
2023-12-12 13:24:21 +0530

    Add ethers library and update dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
 	<div class="m-auto container">
-		<div class="flex min-h-screen">
-			<div class="w-1/3 m-auto p-10" @mouseenter="getbitcoinPrice()">
+		<div class="flex">
+			<div class="w-1/3 m-auto p-10" v-if="bossman !== null">
 				<h2 class="text-xl md:text-3xl font-bold">
-					<div v-if="rank !== null">#{{ rank }}</div>
-					<div v-if="rank == null">✨✨✨</div>
+					<div># {{ rank - 1 }}</div>
+				</h2>
+
+				<div class="my-5"></div>
+
+				<img :src="`https://robohash.org/` + bossman + `.png?set=set2&size=500x500`" />
+
+				<div class="my-5"></div>
+
+				<input class="p-4 w-full bg-orange-300 hover:bg-orange-400 rounded-lg text-center text-gray-800" v-model="bossman" />
+
+				<div class="my-5"></div>
+
+				<p class="text-xl font-bold bg-orange-300 p-3 rounded-lg text-center">BTC : {{ bossmanBal / 10 ** 8 }}</p>
+			</div>
+
+			<div class="w-1/3 m-auto p-10">
+				<h2 class="text-xl md:text-3xl font-bold">
+					<div v-if="rank !== null"># {{ rank }}</div>
 				</h2>
 
 				<div class="my-5"></div>
@@ -16,17 +33,15 @@
 
 				<div class="my-5"></div>
 
-				<button class="w-full bg-orange-300 px-4 py-3 rounded-lg text-bold hover:bg-orange-400" @click="getBalance">FETCH</button>
+				<p class="text-xl font-bold bg-orange-300 p-3 rounded-lg text-center">BTC : {{ balance / 10 ** 8 }}</p>
+
+
+				<button class="mt-5 w-full bg-orange-300 px-4 py-3 rounded-lg text-bold hover:bg-orange-400" @click="getBalance" v-if="!bossmanBal">FETCH</button>
 			</div>
-			<div class="w-2/3 m-auto p-10">
+
+			<div class="w-1/3 m-auto p-10">
 				<div class="text-center max-w-lg mx-auto">
-					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="balance !== null">Balance: {{ balance / 10 ** 8 }} BTC</p>
-
 					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="balance !== null">Balance: {{ (balance / 10 ** 8) * bitcoinPrice }} USD</p>
-
-					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="bossman !== null">
-						Bossman: {{ bossman }} - Boss Balance {{ bossmanBal / 10 ** 8 }} BTC - Bossman By {{ (bossmanBal - balance) / 10 ** 8 }} BTC
-					</p>
 
 					<p class="mb-7 text-lg text-gray-600 font-medium" v-if="bitcoinPrice !== null">Bitcoin Price: {{ bitcoinPrice }}</p>
 				</div>
@@ -46,7 +61,11 @@ let bossman = ref(null);
 let bossmanBal = ref(null);
 let bitcoinPrice = ref(null);
 
-getbitcoinPrice();
+setInterval(() => {
+	if (Math.random() > 0.5) {
+		getbitcoinPrice();
+	}
+}, 3000);
 
 if (localStorage.getItem("address")) {
 	address.value = localStorage.getItem("address");

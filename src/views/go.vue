f95a095e4054457fc220cfb6a85690d5ef301890
besoeasy <besoeasy@users.noreply.github.com>
2023-12-14 22:17:38 +0530

    kj

--- src/views/go.vue
+++ src/views/go.vue
@@ -9,116 +9,23 @@
 		</div>
 	</section>
 
-	<section class="bg-white py-24 overflow-hidden" v-if="bossmans.length > 5">
-		<div class="container px-4 mx-auto">
-			<h2 class="mt-8 mb-24 text-3xl md:text-6xl font-heading font-medium text-center">Your Competion</h2>
-			<div class="relative flex flex-wrap -mx-4 mb-20 lg:mb-32">
+	<section>
+		<div class="m-auto container my-20" v-if="bossmans.length > 5">
+			<div class="flex flex-wrap -mx-4">
 				<div
-					class="hidden md:block absolute top-0 left-0 ml-40 mt-10 py-6 w-72 lg:w-60 border-t-2 border-r-2 border-blue-500 rounded-tr-xl z-10"
-				></div>
-				<div
-					class="hidden lg:block absolute top-0 left-1/2 ml-40 mt-10 py-6 w-60 border-t-2 border-r-2 border-blue-500 rounded-tr-xl z-10"
-				></div>
-				<div
-					class="hidden md:block lg:hidden absolute top-1/2 left-0 ml-40 mt-20 py-6 w-72 border-t-2 border-r-2 border-blue-500 rounded-tr-xl z-10"
-				></div>
-				<div class="relative w-full md:w-1/2 lg:w-1/4 px-4 mb-20 md:mb-0 bg-white">
+					v-for="(bossman, index) in bossmans"
+					:key="index"
+					class="relative w-full md:w-1/2 lg:w-1/4 px-4 mb-20 lg:mb-0 md:mt-32 bg-white"
+				>
 					<img
 						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[0] + `.png?set=set2&size=500x500`"
+						:src="`https://robohash.org/` + bossman.address + `.png?set=set2&size=500x500`"
 						alt=""
 					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 1 }}</h3>
+					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - index }}</h3>
 					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[0] }}
-					</p>
-				</div>
-				<div class="relative w-full md:w-1/2 lg:w-1/4 px-4 mb-20 lg:mb-0 md:mt-32 bg-white">
-					<img
-						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[1] + `.png?set=set2&size=500x500`"
-						alt=""
-					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 2 }}</h3>
-					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[1] }}
-					</p>
-				</div>
-				<div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-20 md:mb-0 bg-white">
-					<img
-						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[2] + `.png?set=set2&size=500x500`"
-						alt=""
-					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 3 }}</h3>
-					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[2] }}
-					</p>
-				</div>
-				<div class="w-full md:w-1/2 lg:w-1/4 px-4 md:mt-32 bg-white">
-					<img
-						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[3] + `.png?set=set2&size=500x500`"
-						alt=""
-					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 4 }}</h3>
-					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[3] }}
-					</p>
-				</div>
-			</div>
-			<div class="relative flex flex-wrap -mx-4 mb-20 lg:mb-32">
-				<div
-					class="hidden md:block absolute top-0 left-0 ml-40 mt-10 py-6 w-72 lg:w-60 border-t-2 border-r-2 border-blue-500 rounded-tr-xl z-10"
-				></div>
-				<div
-					class="hidden lg:block absolute top-0 left-1/2 ml-40 mt-10 py-6 w-60 border-t-2 border-r-2 border-blue-500 rounded-tr-xl z-10"
-				></div>
-				<div
-					class="hidden md:block lg:hidden absolute top-1/2 left-0 ml-40 mt-20 py-6 w-72 border-t-2 border-r-2 border-blue-500 rounded-tr-xl z-10"
-				></div>
-				<div class="relative w-full md:w-1/2 lg:w-1/4 px-4 mb-20 md:mb-0 bg-white">
-					<img
-						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[4] + `.png?set=set2&size=500x500`"
-						alt=""
-					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 5 }}</h3>
-					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[4] }}
-					</p>
-				</div>
-				<div class="relative w-full md:w-1/2 lg:w-1/4 px-4 mb-20 lg:mb-0 md:mt-32 bg-white">
-					<img
-						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[5] + `.png?set=set2&size=500x500`"
-						alt=""
-					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 6 }}</h3>
-					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[5] }}
-					</p>
-				</div>
-				<div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-20 md:mb-0 bg-white">
-					<img
-						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[6] + `.png?set=set2&size=500x500`"
-						alt=""
-					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 7 }}</h3>
-					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[6] }}
-					</p>
-				</div>
-				<div class="w-full md:w-1/2 lg:w-1/4 px-4 md:mt-32 bg-white">
-					<img
-						class="h-32 w-32 mb-12 rounded-lg"
-						:src="`https://robohash.org/` + bossmans[7] + `.png?set=set2&size=500x500`"
-						alt=""
-					/>
-					<h3 class="mb-2 text-3xl font-medium">#{{ myrank - 8 }}</h3>
-					<p class="text-base text-gray-400 leading-relaxed">
-						{{ bossmans[7] }}
+						Address: {{ bossman.address }}<br />
+						Balance: {{ bossman.humanbal }}
 					</p>
 				</div>
 			</div>
@@ -155,21 +62,15 @@
 
 		console.log(data.balance);
 
-		return data.balance;
+		return { bal: data.balance, humanbal: data.balance / 10 ** 8 };
 	}
 
 	async function findBalPos(address) {
-		const balance = await getBitcoinBalance(address);
+		const { bal, humanbal } = await getBitcoinBalance(address);
 
-		const exp2 = await axiosCall(`https://api.blockchair.com/bitcoin/addresses?a=count()&q=balance(${balance}..34859739823342)`);
+		const exp2 = await axiosCall(`https://api.blockchair.com/bitcoin/addresses?a=count()&q=balance(${bal}..34859739823342)`);
 
-		const rank = exp2.data[0]['count()'] || 0;
-
-		return {
-			bal: balance,
-			rank: rank,
-			humanbal: balance / 10 ** 8,
-		};
+		return exp2.data[0]['count()'] || 0;
 	}
 
 	async function getBossman(balance) {
@@ -192,17 +93,22 @@
 
 	const fetchData = async () => {
 		try {
-			mybalance.value = (await getBitcoinBalance(btcaddress)) / 10 ** 8;
+			const { bal, humanbal } = (await getBitcoinBalance(btcaddress)) / 10 ** 8;
 
-			const { bal, rank, humanbal } = await findBalPos(btcaddress);
+			mybalance.value = bal;
 
-			myrank.value = rank || 0;
+			myrank.value = (await findBalPos(btcaddress)) || 0;
 
 			const bossmanx = await getBossman(bal + 1);
 
 			console.log(bossmanx);
 
-			bossmans.value = bossmanx;
+			for (const address of bossmanx) {
+				const { bal, humanbal } = await getBitcoinBalance(address);
+				bossmans.value.push({ address, bal, humanbal });
+			}
+
+			console.log(bossmans);
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		}

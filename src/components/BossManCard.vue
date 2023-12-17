67559bc4d0d08f13e49ebb016c777016f1258b33
besoeasy <besoeasy@users.noreply.github.com>
2023-12-17 15:06:55 +0530

    Refactor BossManCard and ExternalLinks components

--- src/components/BossManCard.vue
+++ src/components/BossManCard.vue
@@ -1,11 +1,6 @@
 <template>
 	<div class="py-40 m-auto container">
 		<div class="mb-20 max-w-2xl mx-auto text-center">
-			<span
-				class="inline-block mb-5 px-3 py-1.5 text-sm text-white uppercase tracking-tight font-semibold bg-gray-600 rounded-full"
-				style="background: url('https://shuffle.dev/basko-assets/images/gradient.png'); background-repeat: no-repeat; background-size: cover"
-				>Your Competition</span
-			>
 			<h2 class="font-heading text-3xl tracking-tighter">Wallets that scored more than you</h2>
 		</div>
 		<section class="grid w-full grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -18,9 +13,14 @@
 					>
 						<p class="font-medium text-xl"># {{ myrank - index - 1 }}</p>
 
-						<p class="bg-green-100 px-2" v-if="hovered[index]">+ {{ parseFloat(bossman.humanbal - mybalance).toFixed(8) }} BTC</p>
+						<p class="bg-green-100 px-2" v-if="hovered[index]">
+							+ {{ parseFloat(bossman.humanbal - mybalance).toFixed(8) }} BTC
+						</p>
 
-						<img class="w-full h-full" :src="`https://robohash.org/` + bossman.address + `.png?set=set2&size=500x500`" />
+						<img
+							class="w-full h-full"
+							:src="`https://robohash.org/` + bossman.address + `.png?set=set2&size=500x500`"
+						/>
 
 						<div class="my-3"></div>
 
@@ -32,7 +32,7 @@
 							{{ bossman.address }}
 						</p>
 
-						<p class="text-sm bg-yellow-200 text-gray-600" v-if="hovered[index]">
+						<p class="text-xs bg-yellow-200 text-gray-600" v-if="hovered[index]">
 							{{ timeAgo(bossman.date) }}
 						</p>
 					</div>
@@ -43,26 +43,26 @@
 </template>
 
 <script setup>
-import { RouterLink } from "vue-router";
+	import { RouterLink } from 'vue-router';
 
-import { ref } from "vue";
+	import { ref } from 'vue';
 
-import { timeAgo } from "@/func.js";
+	import { timeAgo } from '@/func.js';
 
-let hovered = ref([]);
+	let hovered = ref([]);
 
-const { bossmans, myrank, mybalance } = defineProps({
-	bossmans: {
-		type: Array,
-		required: true,
-	},
-	myrank: {
-		type: Number,
-		required: true,
-	},
-	mybalance: {
-		type: Number,
-		required: true,
-	},
-});
+	const { bossmans, myrank, mybalance } = defineProps({
+		bossmans: {
+			type: Array,
+			required: true,
+		},
+		myrank: {
+			type: Number,
+			required: true,
+		},
+		mybalance: {
+			type: Number,
+			required: true,
+		},
+	});
 </script>
--- src/components/ExternalLinks.vue
+++ src/components/ExternalLinks.vue
@@ -6,11 +6,7 @@
 				<div class="w-full md:w-1/2 lg:w-1/4 xl:w-1/5 px-2 mb-6 xl:mb-0">
 					<div class="flex items-center py-4 px-5 bg-gray-50 rounded-9xl">
 						<div class="flex items-center justify-center bg-white w-16 h-16 rounded-5xl shadow-sm">
-							<img
-								class="h-8"
-								src="https://static.shuffle.dev/uploads/files/52/522012c3f1fb8e26ae3db43d4ca84f5911dec6df/bitcoin-btc-logo.png"
-								alt=""
-							/>
+							<img class="h-8" src="@/assets/btc.png" alt="" />
 						</div>
 						<a :href="`https://bitinfocharts.com/bitcoin/address/` + address" target="_blank">
 							<p class="ml-6 text-xl font-heading font-medium">bitinfocharts.com</p></a
@@ -20,11 +16,7 @@
 				<div class="w-full md:w-1/2 lg:w-1/4 xl:w-1/5 px-2 mb-6 xl:mb-0">
 					<div class="flex items-center py-4 px-5 lg:mt-16 bg-gray-50 rounded-9xl">
 						<div class="flex items-center justify-center w-16 h-16 bg-white rounded-5xl shadow-sm">
-							<img
-								class="h-8"
-								src="https://static.shuffle.dev/uploads/files/52/522012c3f1fb8e26ae3db43d4ca84f5911dec6df/bitcoin-btc-logo.png"
-								alt=""
-							/>
+							<img class="h-8" src="@/assets/btc.png" alt="" />
 						</div>
 						<a :href="`https://oxt.me/address/` + address" target="_blank">
 							<p class="ml-6 text-xl font-heading font-medium">oxt.me</p></a
@@ -34,11 +26,7 @@
 				<div class="hidden xl:block xl:w-1/5 px-2 mb-6 xl:mb-0">
 					<div class="flex items-center py-4 px-5 lg:mt-16 bg-gray-50 rounded-9xl">
 						<div class="flex items-center justify-center w-16 h-16 bg-white rounded-5xl shadow-sm">
-							<img
-								class="h-8"
-								src="https://static.shuffle.dev/uploads/files/52/522012c3f1fb8e26ae3db43d4ca84f5911dec6df/bitcoin-btc-logo.png"
-								alt=""
-							/>
+							<img class="h-8" src="@/assets/btc.png" alt="" />
 						</div>
 						<a :href="`https://blockchair.com/bitcoin/address/` + address" target="_blank">
 							<p class="ml-6 text-xl font-heading font-medium">Blockchair.com</p></a
@@ -48,11 +36,7 @@
 				<div class="w-full md:w-1/2 lg:w-1/4 xl:w-1/5 px-2 mb-6 lg:mb-0">
 					<div class="flex items-center py-4 px-5 lg:mt-16 bg-gray-50 rounded-9xl">
 						<div class="flex items-center justify-center w-16 h-16 bg-white rounded-5xl shadow-sm">
-							<img
-								class="h-8"
-								src="https://static.shuffle.dev/uploads/files/52/522012c3f1fb8e26ae3db43d4ca84f5911dec6df/bitcoin-btc-logo.png"
-								alt=""
-							/>
+							<img class="h-8" src="@/assets/btc.png" alt="" />
 						</div>
 						<a :href="`https://blockchain.info/address/` + address" target="_blank">
 							<p class="ml-6 text-xl font-heading font-medium">Blockchain.info</p></a
@@ -62,11 +46,7 @@
 				<div class="w-full md:w-1/2 lg:w-1/4 xl:w-1/5 px-2 mb-6 lg:mb-0">
 					<div class="flex items-center py-4 px-5 bg-gray-50 rounded-9xl">
 						<div class="flex items-center justify-center w-16 h-16 bg-white rounded-5xl shadow-sm">
-							<img
-								class="h-8"
-								src="https://static.shuffle.dev/uploads/files/52/522012c3f1fb8e26ae3db43d4ca84f5911dec6df/bitcoin-btc-logo.png"
-								alt=""
-							/>
+							<img class="h-8" src="@/assets/btc.png" alt="" />
 						</div>
 						<a :href="`https://explorer.btc.com/btc/address/` + address" target="_blank">
 							<p class="ml-6 text-xl font-heading font-medium">btc.com</p>
